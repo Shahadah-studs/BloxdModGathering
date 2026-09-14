32b85f4a06e4af0661d87989cf181d7d669ba39d
Shahadah Studios Elite <muhammad.ahmed@ecacs16.ab.ca>
2026-09-14 09:21:48 -0600

    Add fire-themed items with custom attributes

---
+++ mods/fireelementcodeblock.js
@@ -0,0 +1,17 @@
+api.giveItem(myId, "Gold Sword", 1, {
+  customDisplayName: "Fire Sword",
+  customDescription: "Burns target players and mobs upon damage",
+  customAttributes: {}
+})
+
+api.giveItem(myId, "Orange Paintball", 1, {
+  customDisplayName: "Fire Burst",
+  customDescription: "Right-click to unleash a 3-block radius fire burst",
+  customAttributes: {}
+})
+
+api.giveItem(myId, "Orange Paintball", 1, {
+  customDisplayName: "Flame Jump",
+  customDescription: "Right-click to jump high into the air",
+  customAttributes: {}
+})
