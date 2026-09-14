198942b634ce52b203dc55f22b055490a24c1bca
Shahadah Studios Elite <muhammad.ahmed@ecacs16.ab.ca>
2026-09-14 09:04:48 -0600

    Add freecamcodeblock.js for invisible solid item

---
+++ mods/freecamcodeblock.js
@@ -0,0 +1,9 @@
+api.giveItem(myId, "Invisible Solid", 1, {
+    customDisplayName: "CAMERA",
+    customDescription: "Freecam",
+    customAttributes: {
+        enchantments: {
+        },
+        enchantmentTier: "Tier 1"
+    }
+});
